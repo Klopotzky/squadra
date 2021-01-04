1fb4dbb299ad9a4ea9bf4af0d429009e70ebac15
s441433 <kryklo@st.wmi.amu.edu.pl>
2021-01-04 22:04:12 +0100

    pod Divio

--- squadra/settings.py
+++ squadra/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'ckeditor',
     'addProject',
     'chat',
-    'channels',
     'workflow',
 ]
 
@@ -90,18 +89,8 @@ WSGI_APPLICATION = 'squadra.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-#DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
-}
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
