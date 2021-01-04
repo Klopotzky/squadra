15ba46ed133a3dedb01af0a72d33efa6d0a2a769
s441433 <kryklo@st.wmi.amu.edu.pl>
2021-01-04 22:15:15 +0100

    zmiana bazy na PostgreSQL

--- squadra/settings.py
+++ squadra/settings.py
@@ -90,7 +90,18 @@ WSGI_APPLICATION = 'squadra.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+#DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
